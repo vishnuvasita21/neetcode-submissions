058abb27ce34c3753e3befe0a5c1374357a52965
Vishnu Vasita <118429816+vishnuvasita21@users.noreply.github.com>
2026-07-17 12:58:06 -0400

    Add: merge-k-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-5.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists) {
+        if(lists.length ===0) return null
+        for(let i=1; i<lists.length; i++){
+            lists[0] = this.mergeLists(lists[0], lists[i])
+        }
+
+        return lists[0]
+    }
+
+
+    mergeLists(l1, l2){
+        const dummy = new ListNode()
+        let tail = dummy
+
+
+        while(l1 && l2){
+            if(l1.val < l2.val){
+                tail.next = l1
+                l1=l1.next
+            }else{
+                tail.next = l2
+                l2=l2.next
+            }
+            tail = tail.next 
+        }
+        if(l1){
+             tail.next = l1
+        }
+         if(l2){
+             tail.next = l2
+        }
+
+        return dummy.next
+    }
+}
