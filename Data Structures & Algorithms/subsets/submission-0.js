eb1fb3796e2c02b9550144b83c9b2b461ffc953f
Vishnu Vasita <118429816+vishnuvasita21@users.noreply.github.com>
2026-08-02 14:21:20 -0300

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const res = [];
+        const subset = [];
+        this.dfs(nums, 0, subset, res);
+        return res;
+    }
+
+    dfs(nums, i, subset, res) {
+        if (i >= nums.length) {
+            res.push([...subset]);
+            return;
+        }
+        subset.push(nums[i]);
+        this.dfs(nums, i + 1, subset, res);
+        subset.pop();
+        this.dfs(nums, i + 1, subset, res);
+    }
+}
